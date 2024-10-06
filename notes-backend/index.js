754563e8c41cac6e17a40353c29549e8adb39018
GopiWebDev <gopisv.webdev@gmail.com>
2024-10-06 18:36:11 +0530

    test

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,96 +1,81 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+
+const Note = require('./models/note');
+require('dotenv').config();
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'));
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>');
-});
-
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((n) => n.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) response.json(note);
+      else response.status(404).end();
+    })
+    .catch((err) => next(err));
+});
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((res) => {
+      response.status(204).end();
+    })
+    .catch((err) => next(err));
+});
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body;
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((err) => next(err));
+});
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((err) => next(err));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
-});
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((n) => n.id !== id);
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
+  next(error);
 };
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    });
-  }
-
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  };
-
-  notes = notes.concat(note);
-
-  response.json(note);
-});
-
-app.put('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const body = request.body;
-  
-  const note = notes.find((n) => n.id === id);
-  if (!note) {
-    return response.status(404).json({ error: 'Note not found' });
-  }
-
-  const updatedNote = { ...note, important: body.important };
-
-  notes = notes.map((n) => (n.id === id ? updatedNote : n));
-
-  response.json(updatedNote);
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.use(errorHandler);
--- notes-backend/mongo.js
+++ notes-backend/mongo.js
@@ -1,25 +1,3 @@
-const mongoose = require('mongoose');
-
-if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://fullStackOpen:${password}@full-stack-open.etkld.mongodb.net/noteApp?retryWrites=true&w=majority&appName=full-stack-open`;
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-const Note = mongoose.model('Note', noteSchema);
-
 // const note = new Note({
 //   content: 'HTML is easy',
 //   important: true,
@@ -29,10 +7,3 @@ const Note = mongoose.model('Note', noteSchema);
 //   console.log('note saved!');
 //   mongoose.connection.close();
 // });
-
-Note.find({}).then((result) => {  
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
