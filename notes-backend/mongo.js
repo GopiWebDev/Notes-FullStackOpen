53c027d5dfb087ee6e9731b77d01e79c298c8a65
GopiWebDev <gopisv.webdev@gmail.com>
2024-10-10 11:38:07 +0530

    test

--- notes-backend/mongo.js
+++
@@ -1,9 +0,0 @@
-// const note = new Note({
-//   content: 'HTML is easy',
-//   important: true,
-// });
-
-// note.save().then((result) => {
-//   console.log('note saved!');
-//   mongoose.connection.close();
-// });
